f5e9b9389d74788992ac5fcacaa98bb11971983c
lengyela89 <lengyela@lengyela-laptop>
2015-07-28 20:18:31 +0200

    New configuration model in org.eclipse.incquery.viewmodel project

---
+++ host/org.eclipse.incquery.viewmodel/src/org/eclipse/incquery/viewmodel/configuration/Test.java
@@ -0,0 +1,13 @@
+package org.eclipse.incquery.viewmodel.configuration;
+
+import org.eclipse.incquery.runtime.extensibility.QuerySpecificationRegistry;
+
+public class Test {
+
+	// TODO törölni
+	public static void test() {
+		//((ElementRule)ConfigurationPackage.eINSTANCE.getElementRule()).get
+		//ConfigurationFactory.eINSTANCE.create(null).eS
+	}
+	
+}
